fa4d7df8302507149fedd67bb4d60c98cd85497c
jonsax6 <jdc.saxjunky@gmail.com>
2021-07-19 11:11:20 -0400

    added production url to config.js

--- app/config.js
+++ app/config.js
@@ -1,8 +1,8 @@
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
-  development: 'http://localhost:4741'
-}
+  production: "https://tic-tac-toe-api-production.herokuapp.com",
+  development: "http://localhost:4741",
+};
 
 if (window.location.hostname === 'localhost') {
   apiUrl = apiUrls.development
