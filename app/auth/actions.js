ce305d6ae5074520961755fe89a1561e66b0cc14
jonsax6 <jdc.saxjunky@gmail.com>
2021-07-20 16:14:16 -0400

    debuggin...

--- app/auth/actions.js
+++ app/auth/actions.js
@@ -3,13 +3,13 @@ const ui = require('./ui')
 
 const wins = [
   [ 0, 1, 2 ],
-  [ 0, 3, 6 ],
-  [ 0, 4, 8 ],
-  [ 1, 4, 7 ],
-  [ 2, 4, 6 ],
-  [ 2, 5, 8 ],
   [ 3, 4, 5 ],
-  [ 6, 7, 8 ]
+  [ 6, 7, 8 ],
+  [ 0, 3, 6 ],
+  [ 1, 4, 7 ],
+  [ 2, 5, 8 ],
+  [ 0, 4, 8 ],
+  [ 6, 4, 2 ]
 ]
 
 const checkPlayerWin = (n) => {
@@ -53,6 +53,57 @@ const getAllGames = () => {
   api.allGames(store.token)
     .then(ui.onGetAllGamesSuccess)
     .catch(ui.onFailure);
+}
+
+function minimax(newBoard, player) {
+  var availSpots = emptySquares();
+
+  if (checkWin(newBoard, huPlayer)) {
+    return { score: -10 };
+  } else if (checkWin(newBoard, aiPlayer)) {
+    return { score: 10 };
+  } else if (availSpots.length === 0) {
+    return { score: 0 };
+  }
+  var moves = [];
+  for (var i = 0; i < availSpots.length; i++) {
+    var move = {};
+    move.index = newBoard[availSpots[i]];
+    newBoard[availSpots[i]] = player;
+
+    if (player == aiPlayer) {
+      var result = minimax(newBoard, huPlayer);
+      move.score = result.score;
+    } else {
+      var result = minimax(newBoard, aiPlayer);
+      move.score = result.score;
+    }
+
+    newBoard[availSpots[i]] = move.index;
+
+    moves.push(move);
+  }
+
+  var bestMove;
+  if (player === aiPlayer) {
+    var bestScore = -10000;
+    for (var i = 0; i < moves.length; i++) {
+      if (moves[i].score > bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
+  } else {
+    var bestScore = 10000;
+    for (var i = 0; i < moves.length; i++) {
+      if (moves[i].score < bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
+  }
+
+  return moves[bestMove];
 }
 
 
